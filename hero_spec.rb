6596593d285f34c1258b7babc8a6be7288da9f91
vadim <you@example.com>
2017-04-24 09:59:42 +0300

    Refactoring tests

--- hero_spec.rb
+++ hero_spec.rb
@@ -1,25 +1,24 @@
 require './hero'
 describe Hero do
 
+  before do
+    @hero = Hero.new 'vadim'
+  end
+
   it "has a capitalize name" do
-    hero = Hero.new 'vadim'
-    expect(hero.name).to eq 'Vadim'
+    expect(@hero.name).to eq 'Vadim'
   end
 
   it "can power up" do
-    hero = Hero.new 'vadim'
-
-    expect(hero.power_up).to eq 110
+    expect(@hero.power_up).to eq 110
   end
 
   it "can power down " do
-    hero = Hero.new 'vadim'
-    expect(hero.power_down).to eq 90
+    expect(@hero.power_down).to eq 90
   end
 
   it "display full hero info" do
-    hero = Hero.new 'vadim'
-    expect(hero.hero_info).to eq 'Vadim has a health of 100'
+    expect(@hero.hero_info).to eq 'Vadim has a health of 100'
   end
 
 end
