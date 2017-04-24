b1d1a8d7be559c2c19362e8a8be1d6ef5b42eb11
vadim <you@example.com>
2017-04-24 13:21:24 +0300

    Xerror

---
+++ car.rb
@@ -0,0 +1,30 @@
+class Car
+
+  MILES_PER_GALLON = 20
+
+  attr_reader :fuel
+
+  def inizialize
+
+    @fuel = 0
+
+  end
+
+  def add_fuel amount
+
+    @fuel = amount
+
+  end
+
+ 
+  def range
+    0
+  end
+
+end
+
+car = Car.new
+
+car.add_fuel 10
+
+puts "Range is #{car.range}"
